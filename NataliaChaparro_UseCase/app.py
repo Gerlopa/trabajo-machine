b8e5422a1fa73ac1b86786dde3dba96d935fa7ac
cordero20-droid <natacordero243@gmail.com>
2026-03-25 01:50:34 -0500

    Agrego carpeta con mis archivos

--- NataliaChaparro_UseCase/app.py
+++ NataliaChaparro_UseCase/app.py
@@ -2,28 +2,25 @@ from flask import Flask, render_template, request
 import LinearRegression
 import LogisticRegressionModel
 
-
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# @app.route ('/')
-# def home():
-#   return ('Welcome to Machine Learning') 
-
 @app.route('/FirstPage/<name>')
 def FirstPage(name=''):
     return render_template('FirstPage.html', name=name)
 
-@app.route("/LinearRegression/", methods = ["GET", "POST"])
+
+@app.route("/LinearRegression/", methods=["GET", "POST"])
 def calculateGrade():
-    calculateGrade = None 
+    result = None 
     if request.method == "POST":
         hours = float(request.form["hours"])
-        calculateGrade = LinearRegression.calculateGrade(hours)
-    return render_template("linearRegressionGrade.html", result = calculateGrade)
+        result = LinearRegression.calculateGrade(hours)
+    return render_template("linearRegressionGrade.html", result=result)
+
 
 @app.route('/logisticRegression', methods=["GET", "POST"])
 def logisticRegression():
@@ -41,3 +38,16 @@ def logisticRegression():
         result = LogisticRegressionModel.predict(data)
 
     return render_template("logisticRegression.html", result=result)
+
+
+@app.route('/linear_basic')
+def linear_basic():
+    return render_template('linear_basic.html')
+
+@app.route('/linear_application', methods=['GET', 'POST'])
+def linear_application():
+    result = None
+    if request.method == "POST":
+        hours = float(request.form["hours"])
+        result = LinearRegression.calculateGrade(hours)
+    return render_template("linear_application.html", result=result)
