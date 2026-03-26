bb5a2aca50002f6350c2027adab853b3a1b7839f
Gerlopa <elweydelpanxd@gmail.com>
2026-03-25 20:50:11 -0500

    Refactor app.py to streamline routes and logic

--- regresion/app.py
+++ regresion/app.py
@@ -8,22 +8,6 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/case1")
-def case1():
-    return render_template("case1.html")
-
-@app.route("/case2")
-def case2():
-    return render_template("case2.html")
-
-@app.route("/case3")
-def case3():
-    return render_template("case3.html")
-
-@app.route("/case4")
-def case4():
-    return render_template("case4.html")
-
 @app.route("/regression", methods=["GET", "POST"])
 def regression_view():
     prediction = None
