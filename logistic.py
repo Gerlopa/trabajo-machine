305ec570034df48d01c6836efd81ed507f0591d2
Gerlopa <elweydelpanxd@gmail.com>
2026-04-25 09:11:54 -0500

    Update logistic.py

--- logistic.py
+++ logistic.py
@@ -37,9 +37,6 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-#SMOTE para balancear clases en el entrenamiento
-sm = SMOTE(random_state=42)
-X_train_res, y_train_res = sm.fit_resample(X_train, y_train)
 
 # Modelo
 model = LogisticRegression(max_iter=1000)
